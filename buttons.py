0c4297c5859b84eb538293e4c191c83b54241e12
Nurulloh <nurulloh637@gmail.com>
2025-07-18 15:19:51 +0500

    menu buttoni qoshdik

---
+++ buttons.py
@@ -0,0 +1,10 @@
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton
+
+def menu():
+    markup = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+    btn1 = KeyboardButton("Fantastik kinolar")
+    btn2 = KeyboardButton("Komediya kinolar")
+    btn3 = KeyboardButton("Drama kinolar")
+    btn4 = KeyboardButton("Triler kinolar")
+    markup.add(btn1, btn2, btn3, btn4)
+    return markup
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telebot import TeleBot
 from telebot.types import Message
+from buttons import menu
+
 
 TOKEN = "7713683280:AAGPhVKFz-b1qQCdB1cXHDHBjKMY4fon4mo"
 
@@ -9,7 +11,8 @@ bot = TeleBot(TOKEN)
 def send_welcome(message: Message):
     chat_id = message.chat.id
     full_name = message.chat.first_name
-    bot.send_message(chat_id, f"Assalomu alykum {full_name} hush kelibsiz")
+    bot.send_message(chat_id, f"Assalomu alykum {full_name} hush kelibsiz",
+                     reply_markup=menu())
 
 @bot.message_handler(commands=['help'])
 def send_help(message: Message):
